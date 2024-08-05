9689ec7aaf1f5cd2973ab394d631ead1c3999d8d
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-08-06 00:26:27 +0300

    feat: update obols info

--- src/routes/obols/route.tsx
+++ src/routes/obols/route.tsx
@@ -1,16 +1,17 @@
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { AlertCircle } from "lucide-react";
 
 export default function ObolsRoute() {
   return (<>
     <Alert variant="default">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Information still being gathered</AlertTitle>
-          <AlertDescription>
-            Different level Obols provides different amount of points towards power up. Information will be added as it's found out
-          </AlertDescription>
-        </Alert>
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Information still being gathered</AlertTitle>
+      <AlertDescription>
+        Different level Obols provides different amount of points towards power up. Information will be added as it's found out
+      </AlertDescription>
+    </Alert>
+    <h2 className="text-2xl">Stats increases</h2>
     <Table>
       <TableHeader>
         <TableRow>
@@ -19,36 +20,83 @@ export default function ObolsRoute() {
           <TableHead>Points Required</TableHead>
           <TableHead>Total Points Required</TableHead>
           <TableHead>Color</TableHead>
+          <TableHead>Points Provided towards power-up</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         <TableRow>
-          <TableHead>1</TableHead>
-          <TableHead>+1</TableHead>
-          <TableHead>-</TableHead>
-          <TableHead>-</TableHead>
-          <TableHead>Grey</TableHead>
+          <TableCell>1</TableCell>
+          <TableCell>+1</TableCell>
+          <TableCell>-</TableCell>
+          <TableCell>-</TableCell>
+          <TableCell>Grey</TableCell>
+          <TableCell>1</TableCell>
         </TableRow>
         <TableRow>
-          <TableHead>2</TableHead>
-          <TableHead>+2</TableHead>
-          <TableHead>4</TableHead>
-          <TableHead>4</TableHead>
-          <TableHead>Gold</TableHead>
+          <TableCell>2</TableCell>
+          <TableCell>+2</TableCell>
+          <TableCell>4</TableCell>
+          <TableCell>4</TableCell>
+          <TableCell>Gold</TableCell>
+          <TableCell>4</TableCell>
         </TableRow>
         <TableRow>
-          <TableHead>3</TableHead>
-          <TableHead>+4</TableHead>
-          <TableHead>14</TableHead>
-          <TableHead>18</TableHead>
-          <TableHead>Emerald</TableHead>
+          <TableCell>3</TableCell>
+          <TableCell>+4</TableCell>
+          <TableCell>14</TableCell>
+          <TableCell>18</TableCell>
+          <TableCell>Emerald</TableCell>
+          <TableCell>?</TableCell>
         </TableRow>
         <TableRow>
-          <TableHead>4</TableHead>
-          <TableHead>+?</TableHead>
-          <TableHead>41</TableHead>
-          <TableHead>59</TableHead>
-          <TableHead>Ruby</TableHead>
+          <TableCell>4</TableCell>
+          <TableCell>+7</TableCell>
+          <TableCell>41</TableCell>
+          <TableCell>59</TableCell>
+          <TableCell>Ruby</TableCell>
+          <TableCell>?</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>5</TableCell>
+          <TableCell>+?</TableCell>
+          <TableCell>104</TableCell>
+          <TableCell>163</TableCell>
+          <TableCell>Purple</TableCell>
+          <TableCell>?</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+    <h2 className="text-2xl">Drop rates</h2>
+    <div>First time completion is _always_ 100% drop</div>
+    <div>Regular levels - 3x Level Obols - 18</div>
+    <div>Every 5th level - </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Levels</TableHead>
+          <TableHead>Obol Level</TableHead>
+          <TableHead>Drop amount</TableHead>
+          <TableHead>Drop rate on repeated defeat</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>Regular levels</TableCell>
+          <TableCell>1 (Grey)</TableCell>
+          <TableCell>3</TableCell>
+          <TableCell>16%</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Every 5th</TableCell>
+          <TableCell>1 (Grey)</TableCell>
+          <TableCell>2</TableCell>
+          <TableCell>18%</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Every 20th</TableCell>
+          <TableCell>1 (Gold)</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>20%</TableCell>
         </TableRow>
       </TableBody>
     </Table>
